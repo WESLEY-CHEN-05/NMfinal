b1f2376cb73afe55debdc2c34fcb7b77d812a807
Peter <peter920508@gmail.com>
2024-06-09 10:03:12 +0800

    graphql

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,13 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { PageProvider } from './hooks/usePage';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
+// Create an Apollo Client instance
+const client = new ApolloClient({
+  uri: 'http://localhost:4000', // Your GraphQL endpoint
+  cache: new InMemoryCache(),
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <ApolloProvider client={client}>
       <PageProvider>
         <App />
       </PageProvider>
+    </ApolloProvider>
   </React.StrictMode>
 );
 
