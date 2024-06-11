227cdb583866cece3293933bfdd35b94c1106054
Wesley Chen <1011.5131001w@gmail.com>
2024-06-11 16:03:00 +0800

    UPDATE: update challenge and validate VP (split to two files)

--- vclib/validateVP.js
+++ vclib/validateVP.js
@@ -4,79 +4,25 @@ const {
 } = pkg;
 import pkg_id from "@iota/identity-wasm/node/index.js";
 const {
-    Duration,
     EdDSAJwsVerifier,
     FailFast,
     IotaIdentityClient,
-    Jwk,
-    JwkMemStore,
-    JwsSignatureOptions,
     JwsVerificationOptions,
     Jwt,
     JwtCredentialValidationOptions,
     JwtCredentialValidator,
-    JwtPresentationOptions,
     JwtPresentationValidationOptions,
     JwtPresentationValidator,
-    KeyIdMemStore,
-    MethodDigest,
-    Presentation,
-    Storage,
     SubjectHolderRelationship,
     Resolver,
-    Timestamp,
 } = pkg_id;
-
-import { iotaResolution } from './resolveDID.js';
 
 // The API endpoint of an IOTA node, e.g. Hornet.
 const API_ENDPOINT = "http://140.112.18.206:14265";
 
-export async function validateVP(nouce, subjectDID, subjectPrivateKey, subjectJwtString){
+export async function validateVP(nonce, proofJwkString){
 
-    const subjectDocument = await iotaResolution(subjectDID);
-    const subjectFragment = "key-1";
-
-    // Create method Digest
-    const subjectMethodDigest = new MethodDigest(subjectDocument.methods()[0]);
-
-    const _subject_jwk_data = ((subjectDocument.methods()[0]).data().toJSON()).publicKeyJwk;
-    const subject_jwk_data = {
-        ..._subject_jwk_data,
-        d: subjectPrivateKey,
-    };
-
-    // Create jwk object, JwkMemStore
-    const subjectJwk = new Jwk(subject_jwk_data);
-    const subjectJwkStore = new JwkMemStore();
-    const subjectKeyID = await subjectJwkStore.insert(subjectJwk);
-
-    // create KeyIDdMemStore
-    const subjectKeyIDStore = new KeyIdMemStore();
-    subjectKeyIDStore.insertKeyId(subjectMethodDigest, subjectKeyID);
-
-    // Create issuer Storage
-    const subjectStorage = new Storage(subjectJwkStore, subjectKeyIDStore);
-
-    // Set expire time
-    const expires = Timestamp.nowUTC().checkedAdd(Duration.minutes(10));
-
-    // Create a Verifiable Presentation from the Credential
-    const credentialJwt = new Jwt(subjectJwtString);
-    const unsignedVp = new Presentation({
-        holder: subjectDocument.id(),
-        verifiableCredential: [credentialJwt],
-    });
-
-    // Create a JWT verifiable presentation using the holder's verification method
-    // and include the requested challenge and expiry timestamp.
-    const presentationJwt = await subjectDocument.createPresentationJwt(
-        subjectStorage,
-        subjectFragment,
-        unsignedVp,
-        new JwsSignatureOptions({ nonce }),
-        new JwtPresentationOptions({ expirationDate: expires }),
-    );
+    const presentationJwt = new Jwt(proofJwkString);
 
     // Verifier receives the Verifiable Presentation and verifies it.
     const jwtPresentationValidationOptions = new JwtPresentationValidationOptions(
@@ -151,9 +97,9 @@ export async function validateVP(nouce, subjectDID, subjectPrivateKey, subjectJw
     console.log(`VP successfully validated`);
 }
 
-const nonce = "475a7984-1bb5-4c4c-a56f-822bccd46440";
-const subjectDID = "did:iota:tst:0xae010b9df3261a233ac572246ca98bd098f415cd1b9611129606f17a0111f62e";
-const subjectPrivateKey = "Q3O9gmepFS6KAl5GpYs2CzZLeacfpZFdKU8JYPdf4Yg";
-const credentialJwtString = "eyJraWQiOiJkaWQ6aW90YTp0c3Q6MHhmZGEyOGJiZjg2MmM5ZWZjYjY3ZDE2Y2E5ODBiMzcwM2QzZWVlODI3ZTgyZDUyZDZhOTc3YTU0NWVjYjJlZjVmI2tleS0xIiwidHlwIjoiSldUIiwiYWxnIjoiRWREU0EifQ.eyJpc3MiOiJkaWQ6aW90YTp0c3Q6MHhmZGEyOGJiZjg2MmM5ZWZjYjY3ZDE2Y2E5ODBiMzcwM2QzZWVlODI3ZTgyZDUyZDZhOTc3YTU0NWVjYjJlZjVmIiwibmJmIjoxNzE4MDg5NDIyLCJqdGkiOiJodHRwczovL3d3dy50YWl3YW50YXhpLmNvbS50dy8iLCJzdWIiOiJkaWQ6aW90YTp0c3Q6MHhhZTAxMGI5ZGYzMjYxYTIzM2FjNTcyMjQ2Y2E5OGJkMDk4ZjQxNWNkMWI5NjExMTI5NjA2ZjE3YTAxMTFmNjJlIiwidmMiOnsiQGNvbnRleHQiOiJodHRwczovL3d3dy53My5vcmcvMjAxOC9jcmVkZW50aWFscy92MSIsInR5cGUiOlsiVmVyaWZpYWJsZUNyZWRlbnRpYWwiLCJUYXhpRHJpdmVyQ3JlZGVudGlhbCJdLCJjcmVkZW50aWFsU3ViamVjdCI6eyJsaWNlbnNlIjoiQ2VydGlmaWVkIFRheGkgRHJpdmVyIiwibmFtZSI6Ildlc2xleSBDaGVuIn19fQ.ib8RFBKr6Ydd_BM_25oJ_y42Rz1B0p63nQjL3xDP0EGnV7zqILThDax2VDUQT7CgctxDUUR1mFih4LA8BlFPCQ";
+// // ================ SAMPLE USAGE ===================
+// const nonce = "475a7984-1bb5-4c4c-a56f-822bccd46440";
+// const proofJwkString = "eyJraWQiOiJkaWQ6aW90YTp0c3Q6MHhhZTAxMGI5ZGYzMjYxYTIzM2FjNTcyMjQ2Y2E5OGJkMDk4ZjQxNWNkMWI5NjExMTI5NjA2ZjE3YTAxMTFmNjJlI2tleS0xIiwidHlwIjoiSldUIiwibm9uY2UiOiI0NzVhNzk4NC0xYmI1LTRjNGMtYTU2Zi04MjJiY2NkNDY0NDAiLCJhbGciOiJFZERTQSJ9.eyJleHAiOjE3MTgwOTMxMjcsImlzcyI6ImRpZDppb3RhOnRzdDoweGFlMDEwYjlkZjMyNjFhMjMzYWM1NzIyNDZjYTk4YmQwOThmNDE1Y2QxYjk2MTExMjk2MDZmMTdhMDExMWY2MmUiLCJ2cCI6eyJAY29udGV4dCI6Imh0dHBzOi8vd3d3LnczLm9yZy8yMDE4L2NyZWRlbnRpYWxzL3YxIiwidHlwZSI6IlZlcmlmaWFibGVQcmVzZW50YXRpb24iLCJ2ZXJpZmlhYmxlQ3JlZGVudGlhbCI6WyJleUpyYVdRaU9pSmthV1E2YVc5MFlUcDBjM1E2TUhobVpHRXlPR0ppWmpnMk1tTTVaV1pqWWpZM1pERTJZMkU1T0RCaU16Y3dNMlF6WldWbE9ESTNaVGd5WkRVeVpEWmhPVGMzWVRVME5XVmpZakpsWmpWbUkydGxlUzB4SWl3aWRIbHdJam9pU2xkVUlpd2lZV3huSWpvaVJXUkVVMEVpZlEuZXlKcGMzTWlPaUprYVdRNmFXOTBZVHAwYzNRNk1IaG1aR0V5T0dKaVpqZzJNbU01WldaallqWTNaREUyWTJFNU9EQmlNemN3TTJRelpXVmxPREkzWlRneVpEVXlaRFpoT1RjM1lUVTBOV1ZqWWpKbFpqVm1JaXdpYm1KbUlqb3hOekU0TURnNU5ESXlMQ0pxZEdraU9pSm9kSFJ3Y3pvdkwzZDNkeTUwWVdsM1lXNTBZWGhwTG1OdmJTNTBkeThpTENKemRXSWlPaUprYVdRNmFXOTBZVHAwYzNRNk1IaGhaVEF4TUdJNVpHWXpNall4WVRJek0yRmpOVGN5TWpRMlkyRTVPR0prTURrNFpqUXhOV05rTVdJNU5qRXhNVEk1TmpBMlpqRTNZVEF4TVRGbU5qSmxJaXdpZG1NaU9uc2lRR052Ym5SbGVIUWlPaUpvZEhSd2N6b3ZMM2QzZHk1M015NXZjbWN2TWpBeE9DOWpjbVZrWlc1MGFXRnNjeTkyTVNJc0luUjVjR1VpT2xzaVZtVnlhV1pwWVdKc1pVTnlaV1JsYm5ScFlXd2lMQ0pVWVhocFJISnBkbVZ5UTNKbFpHVnVkR2xoYkNKZExDSmpjbVZrWlc1MGFXRnNVM1ZpYW1WamRDSTZleUpzYVdObGJuTmxJam9pUTJWeWRHbG1hV1ZrSUZSaGVHa2dSSEpwZG1WeUlpd2libUZ0WlNJNklsZGxjMnhsZVNCRGFHVnVJbjE5ZlEuaWI4UkZCS3I2WWRkX0JNXzI1b0pfeTQyUnoxQjBwNjNuUWpMM3hEUDBFR25WN3pxSUxUaERheDJWRFVRVDdDZ2N0eERVVVIxbUZpaDRMQThCbEZQQ1EiXX19.9E-9Jftjt9mZ_kkswkyhV2jya7eXTCVKBXjZaSAhXBoeoF3VTVDqyEJZrjkxCZlMnt5539hx3vDbYWNsllujBA";
 
-validateVP(nonce, subjectDID, subjectPrivateKey, credentialJwtString).then(() => process.exit()).catch(console.error);
+// validateVP(nonce, proofJwkString).then(() => process.exit()).catch(console.error);
+// // ================ SAMPLE USAGE ===================
